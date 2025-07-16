5209d6f143ad886cda2d1a9903052147c9b2c980
Khadizh <khadizh@Khaddizhs-MacBook-Air.local>
2025-07-16 14:49:06 +0600

    Updating user/urls

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, index
+from .views import UserViewSet, user_list
 
-# ✅ Роутер түзөбүз жана ViewSet кошобуз
+
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-
-    path('index/', index, name='index'),
 ]
